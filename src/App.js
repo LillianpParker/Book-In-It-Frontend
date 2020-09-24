7265b83306a9682f5066337942a1d60ef5e050d1
Lillian Parker <Lillianpparker@outlook.com>
2020-09-24 16:11:34 -0500

    trying again please

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Books from './components/Books.js';
+import axios from 'axios';
 
 
-function App () {
+function App() {
   const [formInputs, updateFormInputs] = useState({
     cover_image: '',
     title: '',
@@ -10,85 +11,78 @@ function App () {
     synopsis: ''
   });
   const [books, setBooks] = useState([])
+
+  const getBooks = async () => {
+    try {
+      const response = await fetch('https://book-in-it.herokuapp.com/')
+      const data = await response.json()
+      console.log(data)
+      setBooks(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(
     ()=>{
-      const callBooks = async ()=>{
-       await getBooks();	
-     }
-     callBooks();
-    }, 
-    []
-    )
+      (
+        async function (){
+           await getBooks();
+        }
+      )()
+    }, [])
 
-const handleChange = (event) => {
-  const updateInput = Object.assign({}, formInputs, { [event.target.id]: event.target.value })
-  updateFormInputs(updateInput)
-}
-
-const handleSubmit  = async (event) =>{
-  event.preventDefault()
-  try{
-    const response = await fetch('https://book-in-it-api.herokuapp.com/books', {
-      body: JSON.stringify(formInputs),
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      }
-    })
-    const data = await response.json()
-    updateFormInputs({
-      cover_image: '',
-      title: '',
-      author: '',
-      synopsis: ''
-    })
-    setBooks([data, ...books])
-  }catch(error){
-    console.error(error)
+  const handleChange = (event) => {
+    const updateInput = Object.assign({}, formInputs, { [event.target.id]: event.target.value })
+    updateFormInputs(updateInput)
   }
-}
 
-const getBooks = async () => {
-  try {
-    const response = await fetch('https://book-in-it-api.herokuapp.com/books')
-    const data = await response.json()
-    console.log(data)
-   } catch(error){
-     console.error(error)
-   }
- }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        'https://book-in-it.herokuapp.com/',
+        formInputs
+      );
+      const createdBook = response.data
+      await updateFormInputs({
+        cover_image: '',
+        title: '',
+        author: '',
+        synopsis: ''
+      })
+      await setBooks([createdBook, ...books])
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-useEffect(()=>{
- (async function (){
-   await getBooks()
-     })()
-   },[])
+
 
   return (
     <div className="App">
       <div className="container">
-          <nav>
-            <h4>Add A New Book</h4>
+        <nav>
+          <h4>Add A New Book</h4>
           <form onSubmit={handleSubmit}>
-          <label htmlFor="cover_image">Cover Image</label>
-            <input type="text" id="cover_image" value={formInputs.cover_image} onChange={handleChange}/>
+            <label htmlFor="cover_image">Cover Image</label>
+            <input type="text" id="cover_image" value={formInputs.cover_image} onChange={handleChange} />
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" value={formInputs.title} onChange={handleChange}/>
+            <input type="text" id="title" value={formInputs.title} onChange={handleChange} />
             <label htmlFor="author">Author</label>
-            <input type="text" id="author" value={formInputs.author} onChange={handleChange}/>
+            <input type="text" id="author" value={formInputs.author} onChange={handleChange} />
             <label htmlFor="synopsis">Synopsis</label>
-            <input type="text" id="synopsis" value={formInputs.synopsis} onChange={handleChange}/>
+            <input type="text" id="synopsis" value={formInputs.synopsis} onChange={handleChange} />
             <input type="submit" className="submit" />
           </form>
         </nav>
         <main>
-        <Books books={books}/>
-          </main>
+          <Books books={books} />
+        </main>
         <aside></aside>
       </div>
-    <footer />
-  </div>
+      <footer />
+    </div>
   );
 }
 
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,34 +1,18 @@
 import React, {useState, useEffect} from 'react';
 
 export default function Books (props) {
-    const [books, setBooks] = useState([])
-    const getBooks = async () => {
-       try {
-         const response = await fetch('https://book-in-it-api.herokuapp.com/books')
-         const data = await response.json()
-         setBooks(data)
-        } catch(error){
-          console.error(error)
-        }
-      } 
-    useEffect(()=>{
-      (async function (){
-        await getBooks()
-          })()
-        },[])
-
-        return (
-            <div>
-                {books.map( book => {
-                    return  (
-                        <div key={book.id} className="book">
-                            <img src={book.cover_image}/>
-                            <h3>{ book.title }</h3>
-                            <small>{book.author }</small>
-                            <p>{ book.synopsis }</p>
-                        </div>
-                    )
-                })}
-            </div>
-        )
+    return (
+        <div>
+            {props.books.map( book => {
+                return  (
+                    <div key={book.id} className="book">
+                        <img src={book.cover_image}/>
+                        <h3>{ book.title }</h3>
+                        <small>{book.author }</small>
+                        <p className="summary">{ book.synopsis }</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
